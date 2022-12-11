4649de5e8cf0e1616a888057607a207ea1c1a43e
abhilasha <abhilasha15597@gmail.com>
2022-12-11 11:58:44 +0530

    new

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -4,8 +4,8 @@ import { Route } from 'react-router-dom';
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path='/'/>
-           <Route path='/home'/>
+            <Route path = "/" />
+           <Route path="/home"/>
            <Route path='*' element={<NotFoundPage/>}/>
     
         </Routes>
