40dc0aa414f6524e59cf92952b3a88123d5d6ad7
abhilasha <abhilasha15597@gmail.com>
2022-12-11 17:20:52 +0530

    new

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import React from 'react-router-dom';
+import { Routes,Route } from 'react-router-dom';
 import { Index } from './Pages/Index';
+import {Home} from './Pages/Home';
+import { NotFound } from './Pages/NotFound';
 
 export const AppRoutes = () => {
     return (
